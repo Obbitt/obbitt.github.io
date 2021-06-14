60e9951c4284cbbc4f3b9dff4ebac1254dde5cf9
Sean Costello <seancosty@outlook.com>
2021-06-14 19:39:02 +0100

    Nicer Code

--- source/boot.asm
+++ source/boot.asm
@@ -90,14 +90,14 @@ jmp $
 Print:
     mov ah, 0x0e
     .Loop:
-    cmp [bx], byte 0
-    je .Exit
+        cmp [bx], byte 0
+        je .Exit
         mov al, [bx]
         int 0x10
         inc bx
         jmp .Loop
     .Exit:
-    ret
+        ret
 
 ; Strings
 W:
